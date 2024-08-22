602b3f9a62aa328d6bfb21809938bedc9dac751f
Safi Ullah <safi06947@gmail.com>
2024-08-22 14:42:46 +0500

    redux configure

---
+++ src/store/authSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    status: false,
+    userData: null
+}
+
+export const userSlice = createSlice({
+    name: "authSlice",
+    initialState,
+    reducers: {
+        login (state, action){
+            state.status = true;
+             state.userData = action.payload.userData;
+        },
+        logout (state){
+            state.status = false;
+            state.userData = null;
+        }
+    }
+})
+
+export const {login, logout} = userSlice.actions;
+
+export default userSlice.reducer;
